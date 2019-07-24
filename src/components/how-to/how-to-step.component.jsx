86cc76dd825d167bb1ffa93ec92561d5523d227e
vdugeri <verem.dugeri@gmail.com>
2019-07-24 10:28:02 +0100

    create howto component

---
+++ src/components/how-to/how-to-step.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './how-to.styles.scss';
+
+export const HowToStep = () => (
+  <div className="how-to">
+    <div className="how-to__image-container">
+      <img src={image} alt="step" />
+    </div>
+  </div>
+)
